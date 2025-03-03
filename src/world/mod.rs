903f64b461990cd878c26851cc386c715c969b96
TZAMSK <alikhankhakimov@outlook.com>
2025-03-03 06:01:10 -0500

    Spawn world

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -3,12 +3,13 @@ mod systems;
 
 use bevy::prelude::*;
 
-use systems::spawn_lights;
+use systems::{spawn_lights, spawn_world_model};
 
 pub struct WorldPlugin;
 
 impl Plugin for WorldPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_lights);
+        app.add_systems(Startup, spawn_lights)
+            .add_systems(Startup, spawn_world_model);
     }
 }
--- src/world/systems.rs
+++ src/world/systems.rs
@@ -2,6 +2,19 @@ use bevy::{prelude::*, render::view::RenderLayers};
 
 use crate::camera::renderlayers::{DEFAULT_RENDER_LAYER, VIEW_MODEL_RENDER_LAYER};
 
+use crate::world::components::World;
+
+pub fn spawn_world_model(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let floor = meshes.add(Plane3d::new(Vec3::Y, Vec2::splat(10.0)));
+    let material = materials.add(Color::WHITE);
+
+    commands.spawn((Mesh3d(floor), MeshMaterial3d(material.clone()), World));
+}
+
 pub fn spawn_lights(mut commands: Commands) {
     commands.spawn((
         PointLight {
